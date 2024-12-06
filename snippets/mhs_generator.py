dbbabaf0150bed5355660feb001859dba750e685
ArenBabikian <aren.babikian@mail.mcgill.ca>
2024-12-06 16:04:30 -0500

    first attempts with mandatory 2 obstacles

--- snippets/mhs_generator.py
+++ snippets/mhs_generator.py
@@ -18,8 +18,13 @@ import csv
 from itertools import chain
 import time
 
+from pymoo.util.ref_dirs import get_reference_directions
+from pymoo.algorithms.moo.nsga3 import NSGA3
+from pymoo.algorithms.moo.nsga2 import NSGA2
+from pymoo.termination.max_time import TimeBasedTermination
 
-NUM_OBJECTIVES: int = 10
+
+NUM_OBJECTIVES: int = 8
 SPEED: int = 100
 
 min_size = Obstacle.Size(2, 2, 10)
@@ -37,14 +42,24 @@ class Execution:
 all_executions: List[Execution] = []
 test_cases: List[TestCase] = []
 
+def denormalize(v, min_val, max_val):
+    val =  min_val + (max_val-min_val)*v
+    return val
+
 class ObstacleParams:
     def __init__(self, x: Dict[str, float], i: int) -> None:
-        self.l = x[f"l{i}"]
-        self.w = x[f"w{i}"]
-        self.h = x[f"h{i}"]
-        self.x = x[f"x{i}"]
-        self.y = x[f"y{i}"]
-        self.r = x[f"r{i}"]
+        # self.l = x[f"l{i}"]
+        # self.w = x[f"w{i}"]
+        # self.h = x[f"h{i}"]
+        # self.x = x[f"x{i}"]
+        # self.y = x[f"y{i}"]
+        # self.r = x[f"r{i}"]
+        self.l = denormalize(x[i*6+0], min_size.l, max_size.l)
+        self.w = denormalize(x[i*6+1], min_size.w, max_size.w)
+        self.h = denormalize(x[i*6+2], min_size.h, max_size.h)
+        self.x = denormalize(x[i*6+3], min_position.x, max_position.x)
+        self.y = denormalize(x[i*6+4], min_position.y, max_position.y)
+        self.r = denormalize(x[i*6+5], min_position.r, max_position.r)
         self.polygon: Polygon = None
         self.corners: List[Tuple[float, float]] = None
 
@@ -54,26 +69,30 @@ class ObstaclePlacementProblem(ElementwiseProblem):
     def __init__(self, case_study: TestCase, starting_point: Point):
         self.case_study = case_study
         self.starting_point = starting_point
-        vars: Dict[str, BoundedVariable] = {}
-        vars["n"] = Integer(bounds=(1, 3))
-        for i in range(3):
-            vars[f"l{i}"] = Real(bounds=(min_size.l, max_size.l))
-            vars[f"w{i}"] = Real(bounds=(min_size.w, max_size.w))
-            vars[f"h{i}"] = Real(bounds=(min_size.h, max_size.h))
-            vars[f"x{i}"] = Real(bounds=(min_position.x, max_position.x))
-            vars[f"y{i}"] = Real(bounds=(min_position.y, max_position.y))
-            vars[f"r{i}"] = Real(bounds=(min_position.r, max_position.r))
-        super().__init__(vars=vars, n_obj=NUM_OBJECTIVES)
+        # vars: Dict[str, BoundedVariable] = {}
+        # vars["n"] = Integer(bounds=(1, 3))
+        # for i in range(2):
+        #     vars[f"l{i}"] = Real(bounds=(min_size.l, max_size.l))
+        #     vars[f"w{i}"] = Real(bounds=(min_size.w, max_size.w))
+        #     vars[f"h{i}"] = Real(bounds=(min_size.h, max_size.h))
+        #     vars[f"x{i}"] = Real(bounds=(min_position.x, max_position.x))
+        #     vars[f"y{i}"] = Real(bounds=(min_position.y, max_position.y))
+        #     vars[f"r{i}"] = Real(bounds=(min_position.r, max_position.r))
+        # super().__init__(vars=vars, n_obj=NUM_OBJECTIVES)
+        super().__init__(n_var=12, n_obj=NUM_OBJECTIVES, n_constr=0, xl=0.0, xu=1.0)
+
 
     def _evaluate(self, x: Dict[str, float], out: Dict[str, List[float]], *args, **kwargs):
-        num_obstacles = x["n"]
+        # num_obstacles = x["n"]
+        num_obstacles = 2
+        # print(x)
 
         # We want to put one obstacle at a time
-        max_num_obstacles = max(e.depth + 1 for e in all_executions)
+        # max_num_obstacles = max(e.depth + 1 for e in all_executions)
 
-        if num_obstacles > max_num_obstacles:
-            out["F"] = [num_obstacles - max_num_obstacles] + [float('inf')] * (NUM_OBJECTIVES - 1)
-            return
+        # if num_obstacles > max_num_obstacles:
+        #     out["F"] = [num_obstacles - max_num_obstacles] + [float('inf')] * (NUM_OBJECTIVES - 1)
+        #     return
 
         obstacle_params: List[ObstacleParams] = []
 
@@ -93,6 +112,7 @@ class ObstaclePlacementProblem(ElementwiseProblem):
 
             obstacle_params.append(o)
 
+        # print("here")
         # Check if all corners are within the allowed bounds after rotation
         # The output is the sum of distances that exceed the boundary, which we want to minimize (0)
         distances_out_of_bound = [sum(max(0, min_position.x - x) + max(0, x - max_position.x) + max(0, min_position.y - y) + max(0, y - max_position.y) for x, y in o.polygon.exterior.coords[:4])
@@ -108,13 +128,14 @@ class ObstaclePlacementProblem(ElementwiseProblem):
         
         # Obstacles should block at least one trajectory at their depth, determined also by the previous obstacles
         min_distances_obstacle_to_trajectory: List[float] = []
-        execs_at_depth = set([e for e in all_executions if e.depth == 0])
+        # execs_at_depth = set([e for e in all_executions if e.depth == 0])
+        execs_at_depth = all_executions
         parent_execs = set()
         for depth in range(num_obstacles):
             # This will be 0 if the trajectory crosses the obstacle
             min_distances_obstacle_to_trajectory.append(min(obstacle_params[depth].polygon.distance(e.testCase.trajectory.to_line()) for e in execs_at_depth))
             parent_execs = execs_at_depth
-            execs_at_depth = set(chain.from_iterable(e.followup for e in execs_at_depth))
+            # execs_at_depth = set(chain.from_iterable(e.followup for e in execs_at_depth))
 
         # Obstacles should be close to each other
         # max_min_distance_to_other_obstacles = \
@@ -132,37 +153,54 @@ class ObstaclePlacementProblem(ElementwiseProblem):
         enforced_heuristics = [distances_out_of_bound[i] + areas_overlapping[i] + min_distances_obstacle_to_trajectory[i] for i in range(num_obstacles)]
 
         enforced_heuristics += [sum(enforced_heuristics) / num_obstacles] * (3 - num_obstacles)
-        sums_dists_to_others += [sum(sums_dists_to_others) / num_obstacles] * (3 - num_obstacles)
 
         if sum(enforced_heuristics) > 1e-7: # \
             # or max_min_distance_to_other_obstacles > 10:
+            # out["F"] = [
+            #     0,
+            #     enforced_heuristics[0],
+            #     enforced_heuristics[1],
+            #     enforced_heuristics[2],
+            #     # sqrt to make it less important, minimize negation to maximize actual distance
+            #     -math.sqrt(sums_dists_to_others[0]),
+            #     -math.sqrt(sums_dists_to_others[1]),
+            #     -math.sqrt(sums_dists_to_others[2]),
+            #     # max_min_distance_to_other_obstacles if sum_distance_out_of_bound + area_overlapping + num_too_close + sum_min_distance_obstacle_to_trajectory < 1e-5 else float('inf'),
+            #     min_distance_to_start,
+            #     num_obstacles,
+            #     float('inf')
+            # ]
             out["F"] = [
                 0,
                 enforced_heuristics[0],
                 enforced_heuristics[1],
-                enforced_heuristics[2],
+                # enforced_heuristics[2],
                 # sqrt to make it less important, minimize negation to maximize actual distance
                 -math.sqrt(sums_dists_to_others[0]),
                 -math.sqrt(sums_dists_to_others[1]),
-                -math.sqrt(sums_dists_to_others[2]),
+                # -math.sqrt(sums_dists_to_others[2]),
                 # max_min_distance_to_other_obstacles if sum_distance_out_of_bound + area_overlapping + num_too_close + sum_min_distance_obstacle_to_trajectory < 1e-5 else float('inf'),
                 min_distance_to_start,
                 num_obstacles,
-                float('inf')
+                100000
             ]
-            print([x[k] for k in params_keys])
-            print("Distances out of bound " + str(distances_out_of_bound))
-            print("Areas overlapping " + str(areas_overlapping))
-            print("Min distances to trajectory " + str(min_distances_obstacle_to_trajectory))
-            print("Min distances to others " + str(sums_dists_to_others))
-            print("Min distance to starting point " + str(min_distance_to_start))
+            # print([x[k] for k in params_keys])
+            # print(x)
+            # print("---")
+            # print("Distances out of bound " + str(distances_out_of_bound))
+            # print("Areas overlapping " + str(areas_overlapping))
+            # print("Min distances to trajectory " + str(min_distances_obstacle_to_trajectory))
+            # print("Min distances to others " + str(sums_dists_to_others))
+            # print("Min distance to starting point " + str(min_distance_to_start))
+
+            # print(out["F"])
         else:
             print("Executing mission with X = " + str(x))
             obstacles_test = [Obstacle(Obstacle.Size(o.l, o.w, o.h), Obstacle.Position(o.x, o.y, 0, o.r)) for o in obstacle_params]
             test = TestCase(self.case_study, obstacles_test)
-            Trajectory(positions = [Position(0, 0, 0, 0, 0), Position(0, 0, 0, 0, 0)]) \
-                .plot(obstacles = obstacles_test)
-            time.sleep(1)
+            # Trajectory(positions = [Position(0, 0, 0, 0, 0), Position(0, 0, 0, 0, 0)]) \
+            #     .plot(obstacles = obstacles_test)
+            # time.sleep(1)
             test.test.speed = SPEED
             test.test.simulation.speed = SPEED
             min_distance = float('inf')
@@ -190,29 +228,47 @@ class ObstaclePlacementProblem(ElementwiseProblem):
                 writer = csv.writer(file)
                 if file.tell() == 0:
                     writer.writerow(params_keys + ["min_distance", "result"])
-                writer.writerow([x[k] for k in params_keys] + [min_distance, result])
-            print([x[k] for k in params_keys])
+                writer.writerow([x[k] for k in range(12)] + [min_distance, result])
+                # writer.writerow([x[k] for k in params_keys] + [min_distance, result])
+            # print([x[k] for k in params_keys])
+            # print(x)
+            print("---")
             print("Distances out of bound " + str(distances_out_of_bound))
             print("Areas overlapping " + str(areas_overlapping))
             print("Min distances to trajectory " + str(min_distances_obstacle_to_trajectory))
             print("Min distances to others " + str(sums_dists_to_others))
             print("Min distance to starting point " + str(min_distance_to_start))
 
+            # out["F"] = [
+            #     0,
+            #     enforced_heuristics[0],
+            #     enforced_heuristics[1],
+            #     enforced_heuristics[2],
+            #     # sqrt to make it less important, minimize negation to maximize actual distance
+            #     -math.sqrt(sums_dists_to_others[0]),
+            #     -math.sqrt(sums_dists_to_others[1]),
+            #     -math.sqrt(sums_dists_to_others[2]),
+            #     # max_min_distance_to_other_obstacles if sum_distance_out_of_bound + area_overlapping + num_too_close + sum_min_distance_obstacle_to_trajectory < 1e-5 else float('inf'),
+            #     min_distance_to_start,
+            #     num_obstacles,
+            #     result ** 3 # Make it super important
+            # ]
             out["F"] = [
                 0,
                 enforced_heuristics[0],
                 enforced_heuristics[1],
-                enforced_heuristics[2],
+                # enforced_heuristics[2],
                 # sqrt to make it less important, minimize negation to maximize actual distance
                 -math.sqrt(sums_dists_to_others[0]),
                 -math.sqrt(sums_dists_to_others[1]),
-                -math.sqrt(sums_dists_to_others[2]),
+                # -math.sqrt(sums_dists_to_others[2]),
                 # max_min_distance_to_other_obstacles if sum_distance_out_of_bound + area_overlapping + num_too_close + sum_min_distance_obstacle_to_trajectory < 1e-5 else float('inf'),
                 min_distance_to_start,
                 num_obstacles,
                 result ** 3 # Make it super important
             ]
 
+            # print(out["F"])
 class MHSGenerator(object):
 
     def __init__(self, case_study_file: str) -> None:
@@ -228,6 +284,7 @@ class MHSGenerator(object):
         print("Executing mission without obstacle")
         try:
             default_test.execute()
+            # default_test.trajectory = Trajectory(positions = [Position(0, 0, 0, 0, 0), Position(0, 0, 0, 0, 0)])
             default_test.plot()
             time.sleep(1)
             print("Finished")
@@ -235,16 +292,22 @@ class MHSGenerator(object):
         except Exception as e:
             print("Exception during test execution, skipping the test")
             print(e)
+            exit()
 
         problem = ObstaclePlacementProblem(self.case_study, Point(default_test.trajectory.to_line().coords[0]))
 
-        algorithm = MixedVariableGA(pop_size=5, n_offsprings=5, survival=RankAndCrowding())
-        # TODO: Change population and offspring sizes, and maybe survival strategy
+        # algorithm = MixedVariableGA(pop_size=50, n_offsprings=100, survival=RankAndCrowding())
+        ref_dirs = get_reference_directions("das-dennis", n_dim=NUM_OBJECTIVES, n_partitions=1)
+        algorithm = NSGA3(ref_dirs=ref_dirs, pop_size=50, n_offsprings=100, eliminate_duplicates=True)
+        # algorithm = NSGA2(pop_size=50, n_offsprings=100)
 
 
         class ObstaclePlacementTermination(Termination):
             def _update(self, algorithm):
-                return 1 if len(all_executions) >= budget else 0
+                r = 1 if len(all_executions) >= budget else 0
+                # print(r)
+                return r
+                return (len(all_executions)+1)/(budget+1)
 
         termination = ObstaclePlacementTermination()
 
